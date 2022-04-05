affb8ae61c0650f00f459768ccfebfb7ab3c4486
Danniel C <dnncervantes.yahoo.com>
2022-04-04 20:08:17 -0700

    first commit

---
+++ CafeUtil.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+public class CafeUtil{
+    public int getStreakGoal(){
+        int sum = 0;
+        for(int i = 0; i <= 10; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    public double getOrderTotal(double[] prices){
+        double sum = 0;
+        for(int i = 0; i<prices.length; i++){
+            sum += prices[i];
+        }
+        return sum;
+    }
+    public void displayMenu(ArrayList<String> menu){
+        for(int i = 0; i < menu.size(); i++){
+            System.out.println(i + " " + menu.get(i));
+        }
+    }
+    public void addCustomer(ArrayList<String> customers){
+        System.out.println("Please enter your name:");
+        String userName = System.console().readLine();
+        System.out.printf("Hello " + userName + "! ");
+        System.out.println("There are " + customers.size() + " people in front of you.");
+        customers.add(userName);
+    }
+
+}
